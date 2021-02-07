0da8718d0c00e288f42b2107e2df000f57c1843c
Joanna Piskala <joanna.piskala@gmail.com>
2021-02-07 13:49:06 +0000

    iterating list

--- src/main/java/com/joannap/thymeleafhelloworld/controller/CarController.java
+++ src/main/java/com/joannap/thymeleafhelloworld/controller/CarController.java
@@ -5,6 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 // GUI - graficzny interfejs uzutkownika
 
@@ -15,9 +19,14 @@ public class CarController {
     // musimy zdefiniowac model
     @GetMapping("/car")
     public String getCar(Model model){
-        Car car = new Car ("BMW", "i8");
+        Car car1 = new Car ("BMW", "i8");
+        Car car2 = new Car ("Fiat", "126p");
+        Car car3 = new Car ("Polonez", "Caro");
+        Car car4 = new Car ("Hyundai", "i30");
+        List<Car> cars = Arrays.asList(car1, car2, car3, car4);
+
         model.addAttribute("name", "Joanna");
-        model.addAttribute("car", car);
+        model.addAttribute("cars", cars);
         return "car";
     }
 
