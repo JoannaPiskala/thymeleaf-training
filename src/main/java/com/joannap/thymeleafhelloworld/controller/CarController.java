6c523f052e1fd4ed96a72de79e716eea542cbb65
Joanna Piskala <joanna.piskala@gmail.com>
2021-03-10 17:43:45 +0000

    methods: delete, update

--- src/main/java/com/joannap/thymeleafhelloworld/controller/CarController.java
+++ src/main/java/com/joannap/thymeleafhelloworld/controller/CarController.java
@@ -1,44 +1,74 @@
 package com.joannap.thymeleafhelloworld.controller;
 
 import com.joannap.thymeleafhelloworld.Car;
+import com.joannap.thymeleafhelloworld.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Optional;
 
 
 // GUI - graficzny interfejs uzutkownika
 
 
-@Controller // klasa ktora bedzie zwracala plik html lub plik nalezacy do silnika thymeleaf musi miec adnotacje controller!
+@Controller
 public class CarController {
 
 
-    private List<Car> cars;
+    private CarService carService;
 
-    public CarController() {
-        Car car1 = new Car("BMW", "i8");
-        Car car2 = new Car("Polonez", "Caro");
-        Car car3 = new Car("Hyundai", "i30");
-        cars = new ArrayList<>();
-        cars.add(car1);
-        cars.add(car2);
-        cars.add(car3);
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
     }
 
-    @GetMapping("/car")
-    public String getCar(Model model) {
-        model.addAttribute("cars", cars);
+
+    @GetMapping("/cars")
+    public String showCars(Model model) {
+        model.addAttribute("cars", carService.findAll());
         model.addAttribute("newCar", new Car());
-        return "car";
+        return "index";
     }
+
 
     @PostMapping("/add-car")
     public String addCar(@ModelAttribute Car car) {
-        cars.add(car);
-        return "redirect:/car";
+        carService.addCar(car);
+        return "redirect:/cars";
     }
+
+
+    @GetMapping("/delete/{id}")
+    public String deleteCarById (@PathVariable("id") Long id) {
+        boolean deleted = carService.removeCar(id);
+        if (deleted) {
+            return "redirect:/cars";
+        }
+        return "car not found";
+    }
+
+
+    @GetMapping ("/edit/{id}")
+    public String showUpdateForm(@PathVariable("id") long id, Model model) {
+       Optional<Car> car = carService.findById(id);
+        model.addAttribute("car", car);
+        return "update-car";
+    }
+
+
+    @PostMapping ("/update/{id}")
+    public String updateCar(@PathVariable("id") long id, @ModelAttribute Car car){
+        carService.removeCar(id);
+        carService.addCar(car);
+        car.setId(id);
+        return "redirect:/cars";
+
+    }
+
+
 
 }
